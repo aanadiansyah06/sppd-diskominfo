7929de08b5a728cf04d9429f9e88defbda3f4966
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 11:33:38 +0700

    Create DetailAnggaran Model

--- app/DetailAnggaran.php
+++ app/DetailAnggaran.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class DetailAnggaran extends Model
 {
-    //
+    protected $table = 'detail_anggaran';
+
+    protected $fillable = [
+        'id',
+        'id_anggaran',
+        'id_sppd',
+        'dana',
+    ];
+
+    protected $primaryKey = 'id';
 }
