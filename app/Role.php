fcd838caccf2bbbe78ee83517af9228c9f212fc4
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 13:16:52 +0700

    Create Role Model

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $table = 'role';
+
+    protected $fillable = [
+        'id',
+        'nama_role',
+        'deskripsi_role',
+    ];
+
+    protected $primaryKey = 'id';
 }
