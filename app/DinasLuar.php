7592ed23848f835b662e0e503424c16e08ceaf6a
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 11:40:21 +0700

    Create Dinas Luar Model

--- app/DinasLuar.php
+++ app/DinasLuar.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class DinasLuar extends Model
 {
-    //
+    protected $table = 'dinas_luar';
+
+    protected $fillable = [
+        'id',
+        'tanggal_mulai',
+        'tanggal_selsai',
+        'tujuan',
+        'pic_tujuan',
+        'jumlah_pegawai',
+        'alamat',
+        'kota',
+        'provinsi',
+        'jumlah_anggaran',
+    ];
+
+    protected $primaryKey = 'id';
 }
