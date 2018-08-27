b1da3db47e58160caca7f6177ab90487b7c0f454
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 12:59:39 +0700

    Create OPD Model

--- app/Opd.php
+++ app/Opd.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Opd extends Model
 {
-    //
+    protected $table = 'opd';
+
+    protected $fillable = [
+        'id',
+        'nama_opd',
+        'deskripsi_opd',
+    ];
+
+    protected $primaryKey = 'id';
 }
