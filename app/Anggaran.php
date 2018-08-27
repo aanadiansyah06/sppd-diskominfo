4f43644ca593b75e18ab61974b916fc4c73be57b
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 10:24:49 +0700

    Create Model Anggaran

--- app/Anggaran.php
+++ app/Anggaran.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Anggaran extends Model
 {
-    //
+    protected $table = 'anggaran';
+
+    protected $fillable = [
+        'id',
+        'id_dinas',
+        'tahun',
+        'pagu',
+        'sisa',
+    ];
+
+    protected $primaryKey = 'id';
 }
