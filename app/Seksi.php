6f104f6968ad33b66d8ccd5baba5a80bcac8ada6
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 13:18:15 +0700

    Create Seksi Model

--- app/Seksi.php
+++ app/Seksi.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seksi extends Model
 {
-    //
+    protected $table = 'seksi';
+
+    protected $fillable = [
+        'id',
+        'id_seksi',
+        'nama_seksi',
+        'deskripsi_seksi',
+    ];
+
+    protected $primaryKey = 'id';
 }
