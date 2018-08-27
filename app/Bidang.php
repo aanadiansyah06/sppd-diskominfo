8ff120c359e51f5babe7bfe8b1e78c0ce55a22de
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 10:37:01 +0700

    Create Model Bidang

--- app/Bidang.php
+++ app/Bidang.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bidang extends Model
 {
-    //
+  protected $table = 'bidang';
+
+  protected $fillable = [
+      'id',
+      'id_opd',
+      'nama_bidang',
+      'deskripsi_bidang',
+  ];
+
+  protected $primaryKey = 'id';
 }
