18e45f97c6b6ae51e43d974cb978fadfa87c3b3c
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 13:15:44 +0700

    Create Pegawai Model

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pegawai extends Model
 {
-    //
+    protected $table = 'pegawai';
+
+    protected $fillable = [
+        'id',
+        'nip',
+        'nik',
+        'nama',
+        'telepon',
+        'golongan',
+        'eselon',
+        'jabatan',
+        'id_opd',
+        'id_bidang',
+        'id_seksi',
+    ];
+
+    protected $primaryKey = 'id';
 }
