7631cfea7b8566bc42005b3656aaf7fc33507d62
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 11:52:57 +0700

    Create History Login Model

--- app/HistoryLogin.php
+++ app/HistoryLogin.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class HistoryLogin extends Model
 {
-    //
+  protected $table = 'history_login';
+
+  protected $fillable = [
+      'id',
+      'id_user',
+      'tanggal_login',
+      'jam_login',
+  ];
+
+  protected $primaryKey = 'id';
 }
