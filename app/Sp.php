097af1a22b6866b9cf3977d26b759b73d683a104
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 13:22:16 +0700

    Create SP Model

--- app/Sp.php
+++ app/Sp.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sp extends Model
 {
-    //
+    protected $table = 'sp';
+
+    protected $fillable = [
+        'id',
+        'id_dinas_luar',
+        'id_user',
+        'tanggal_cetak',
+        'jam_cetak',
+    ];
+
+    protected $primaryKey = 'id';
 }
