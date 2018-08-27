4973e556720e0759339cde33b0cadde794d12a30
Ega Javier Harwenda <egaharwenda@gmail.com>
2018-08-27 13:28:12 +0700

    Create Model Sppd

--- app/Sppd.php
+++ app/Sppd.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sppd extends Model
 {
-    //
+    protected $table = 'sppd';
+
+    protected $fillable = [
+        'id',
+        'id_detail_dinas_luar',
+        'id_user',
+        'tanggal_cetak',
+        'jam_cetak',
+    ];
+
+    protected $primaryKey = 'id';
 }
